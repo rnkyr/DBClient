c0ef98bbabd6f8c62649db5eed861693e08d447c
rnkyr <roman.kyrylenko@quarks.tech>
2022-04-01 13:17:29 +0300

    [update] removed synchronous methods

--- DBClient/Core/DBClient.swift
+++ DBClient/Core/DBClient.swift
@@ -65,45 +65,6 @@ public protocol DBClient {
     ///   - objects: objects to be worked with
     ///   - completion: `Result` with inserted and updated instances.
     func upsert<T : Stored>(_ objects: [T], completion: @escaping (Result<(updated: [T], inserted: [T]), DataBaseError>) -> Void)
-    
-    /// Synchronously executes given request and calls completion result wrapped in `Result`.
-    ///
-    /// - Parameters:
-    ///   - request: request to execute
-    /// - Returns: `Result` with array of objects or error in case of failure.
-    func execute<T>(_ request: FetchRequest<T>) -> Result<[T], DataBaseError>
-    
-    /// Synchronously inserts objects to database.
-    ///
-    /// - Parameters:
-    ///   - objects: list of objects to be inserted
-    /// - Returns: `Result` with inserted objects or appropriate error in case of failure.
-    @discardableResult
-    func insert<T: Stored>(_ objects: [T]) -> Result<[T], DataBaseError>
-    
-    /// Synchronously updates changed performed with objects to database.
-    ///
-    /// - Parameters:
-    ///   - objects: list of objects to be updated
-    /// - Returns: `Result` with updated objects or appropriate error in case of failure.
-    @discardableResult
-    func update<T: Stored>(_ objects: [T]) -> Result<[T], DataBaseError>
-    
-    /// Synchronously deletes objects from database.
-    ///
-    /// - Parameters:
-    ///   - objects: list of objects to be deleted
-    /// - Returns: `Result` with appropriate error in case of failure.
-    @discardableResult
-    func delete<T: Stored>(_ objects: [T]) -> Result<Void, DataBaseError>
-    
-    /// Synchronously iterates through given objects and updates existing in database instances or creates them
-    ///
-    /// - Parameters:
-    ///   - objects: objects to be worked with
-    /// - Returns: `Result` with inserted and updated instances.
-    @discardableResult
-    func upsert<T : Stored>(_ objects: [T]) -> Result<(updated: [T], inserted: [T]), DataBaseError>
 }
 
 public extension DBClient {
@@ -114,14 +75,7 @@ public extension DBClient {
     func findAll<T: Stored>(completion: @escaping (Result<[T], DataBaseError>) -> Void) {
         execute(FetchRequest(), completion: completion)
     }
-    
-    /// Synchronously fetch all entities from database
-    ///
-    /// - Returns: `Result` with array of objects
-    func findAll<T: Stored>() -> Result<[T], DataBaseError> {
-        return execute(FetchRequest())
-    }
-    
+
     /// Finds first element with given value as primary.
     /// If no primary key specified for given type, or object with such value doesn't exist returns nil.
     ///
@@ -150,33 +104,6 @@ public extension DBClient {
         }
     }
     
-    /// Synchronously finds first element with given value as primary.
-    /// If no primary key specified for given type, or object with such value doesn't exist returns nil.
-    ///
-    /// - Parameters:
-    ///   - type: type of object to search for
-    ///   - primaryValue: the value of primary key field to search for
-    ///   - predicate: predicate for request
-    /// - Returns: `Result` with found object or nil
-    func findFirst<T: Stored>(_ type: T.Type, primaryValue: String, predicate: NSPredicate? = nil) -> Result<T?, DataBaseError> {
-        guard let primaryKey = type.primaryKeyName else {
-            return .failure(DataBaseError.missingPrimaryKey)
-        }
-        
-        let primaryKeyPredicate = NSPredicate(format: "\(primaryKey) == %@", primaryValue)
-        let fetchPredicate: NSPredicate
-        if let predicate = predicate {
-            fetchPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [primaryKeyPredicate, predicate])
-        } else {
-            fetchPredicate = primaryKeyPredicate
-        }
-        let request = FetchRequest<T>(predicate: fetchPredicate, fetchLimit: 1)
-        
-        let result = execute(request)
-        
-        return result.map { $0.first }
-    }
-    
     /// Inserts object to database.
     ///
     /// - Parameters:
@@ -184,11 +111,6 @@ public extension DBClient {
     ///   - completion: `Result` with inserted object or appropriate error in case of failure.
     func insert<T: Stored>(_ object: T, completion: @escaping (Result<T, DataBaseError>) -> Void) {
         insert([object], completion: { completion($0.next(self.convertArrayTaskToSingleObject)) })
-    }
-    
-    @discardableResult
-    func insert<T: Stored>(_ object: T) -> Result<T, DataBaseError> {
-        return insert([object]).next(convertArrayTaskToSingleObject)
     }
     
     /// Updates changed performed with object to database.
@@ -200,11 +122,6 @@ public extension DBClient {
         update([object], completion: { completion($0.next(self.convertArrayTaskToSingleObject)) })
     }
     
-    @discardableResult
-    func update<T: Stored>(_ object: T) -> Result<T, DataBaseError> {
-        return update([object]).next(convertArrayTaskToSingleObject)
-    }
-    
     /// Deletes object from database.
     ///
     /// - Parameters:
@@ -212,11 +129,6 @@ public extension DBClient {
     ///   - completion: `Result` with appropriate error in case of failure.
     func delete<T: Stored>(_ object: T, completion: @escaping (Result<Void, DataBaseError>) -> Void) {
         delete([object], completion: completion)
-    }
-    
-    @discardableResult
-    func delete<T: Stored>(_ object: T) -> Result<Void, DataBaseError> {
-        return delete([object])
     }
     
     /// Updates existing in database instances or creates them using upsert method defined in your model
@@ -233,17 +145,6 @@ public extension DBClient {
                 
                 return Result.success((object: object, isUpdated: !updated.isEmpty))
             })
-        }
-    }
-    
-    @discardableResult
-    func upsert<T: Stored>(_ object: T) -> Result<(object: T, isUpdated: Bool), DataBaseError> {
-        return upsert([object]).next { (updated: [T], inserted: [T]) in
-            guard let object = updated.first ?? inserted.first else {
-                return Result.failure(DataBaseError.missingData)
-            }
-            
-            return Result.success((object: object, isUpdated: !updated.isEmpty))
         }
     }
     
